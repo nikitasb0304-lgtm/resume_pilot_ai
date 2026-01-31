ac90614c8f2fe45eeed421ccdeaa11b82069fcc2
Nikita Bhandare <nikitasb0304@gmail.com>
2026-01-31 17:26:52 +0530

    useAuth.js

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -4,4 +4,4 @@ import { AuthContext } from '../context/AuthContext'
 export const useAuth = () => {
   const { user, login, logout } = useContext(AuthContext)
   return { user, login, logout }
-}
+} 
